6f497c2f424c5fef9a02b412d09ac3efebb52706
gokul3102 <gokulananthi5602@gmail.com>
2025-04-22 21:51:15 +0530

    Updated config.py to use environment variables

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-import os
-
-EMAIL = os.environ.get('gokulananthi5602@gmail')
-EMAIL_PASSWORD = os.environ.get('tnhc sgsw hpbc fakj')
-ADMIN_USERNAME = os.environ.get('GDA')
-ADMIN_PASSWORD = os.environ.get('GDA@2007')
-
+EMAIL = 'gokulananthi5602@gmail.com'  # Replace with your email
+EMAIL_PASSWORD = 'tnhc sgsw hpbc fakj'  # App password (for Gmail)
+ADMIN_USERNAME = 'GDA'
+ADMIN_PASSWORD = 'GDA@2007'  # Use strong password in production
